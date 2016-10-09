f312434166e97786aa5236d2a64c4e4ee1197c9f
andresosw jiemenz <andres_osw@hotmail.com>
2016-10-09 17:03:54 -0400

    Add business models

--- server/IDCconsultores/impresos/models.py
+++ server/IDCconsultores/impresos/models.py
@@ -1,5 +1,34 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	pdf = models.FileField(upload_to='pdf/')
+	image = models.ImageField(upload_to='img/')
+
+class ProductCategory(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	image = models.ImageField(upload_to='img/')
+
+class Area(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	image = models.ImageField(upload_to='img/')
+
+class Service(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=200)
+	pdf = models.FileField(upload_to='pdf/')
+	image = models.ImageField(upload_to='img/')
+	subservices = models.ManyToManyField(
+		'self',
+		symmetrical=False,
+		through='SupportService',
+		through_fields=('service', 'subservice'),
+		)
+
+class SupportService(models.Model):
+	service = models.ForeignKey(Service,related_name="super_services")
+	subservice = models.ForeignKey(Service,related_name="supported_services")
